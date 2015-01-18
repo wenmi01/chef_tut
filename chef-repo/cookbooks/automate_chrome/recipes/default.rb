5b00343d918c789e59bcf81d8e7c9ad3ea197217
Rowen Remis R. Iral <rowen_remis_iral@yahoo.com>
2015-01-18 03:29:00 -0800

    Updated cookbooks

--- chef-repo/cookbooks/automate_chrome/recipes/default.rb
+++ chef-repo/cookbooks/automate_chrome/recipes/default.rb
@@ -5,14 +5,3 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
-src_npp = "C:\tmp\npp.installer.exe"
-remote_file src_npp do
-  source "http://download.tuxfamily.org/notepadplus/6.7.4/npp.6.7.4.Installer.exe"
-end
-
-#Install Notepad++ using windows_package
-#http://docs.opscode.com/lwrp_windows.html#windows-package
-windows_package "Notepad++" do
-  source 'C:\tmp\npp.installer.exe' 
-  action :install 
-end
--- chef-repo/cookbooks/automate_firefox/recipes/default.rb
+++ chef-repo/cookbooks/automate_firefox/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+powershell_script "Install firefox" do
+  guard_interpreter :powershell_script
+  code <<-EOH
+    (new-object System.Net.WebClient).Downloadfile("http://download.downloadd.co/v2/click/80af325a/?d=http%3A%2F%2Fdownloadd.co%2Fdownload%2Ffirefox.exe&n=Mozilla+FireFox&key=2c4de7f94ef751f16c23013d83473aaf05cff15dbbe3716d87a7e197eaabe046&affiliate_image=&product_image=http%3A%2F%2Fdownloadd.co%2Fdownload%2Ffirefox.png&sid=firefox&filename=Firefox_Setup_26.0", "c:\ff_install.exe")
+     #@START /WAIT "Firefox Installer"  "c:\ff_install.exe" -ms
+     #c:\ff_install.exe -ms
+  EOH
+  action :run
+end
