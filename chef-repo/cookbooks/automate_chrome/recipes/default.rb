a7283625329c421cb766ca5f09c4a7158467e0db
Rowen Remis R. Iral <rowen_remis_iral@yahoo.com>
2015-01-18 18:23:17 -0800

    Fix download installers

--- chef-repo/cookbooks/automate_chrome/recipes/default.rb
+++ chef-repo/cookbooks/automate_chrome/recipes/default.rb
@@ -5,25 +5,13 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
-#msiexec /i "GoogleChromeStandaloneEnterprise.msi" /qn /quiet /norestart
-
 powershell_script "Install Chrome" do
-  #cwd "c:\Windows\Temp\"
+  cwd Chef::Config[:file_cache_path]
+  environment({'chefc' => Chef::Config[:file_cache_path]})
   code <<-EOH
-    echo $pwd
-    $storageDir = $pwd
-    echo $storageDir
-    #$storageDir = "c:\"
-    $webclient = New-Object System.Net.WebClient
-    #$url = "https://teusje.files.wordpress.com/2011/02/giraffe-header1.png"
-    #$file = "$storageDir\myNewFilename.jpg"
-    $url="https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B40569D95-7A9C-D7C3-EAF6-1035625105AE%7D%26lang%3Den%26browser%3D2%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/update2/installers/ChromeSetup.exe"
-    $file="$storageDir\gc_install.exe"
-    $webclient.DownloadFile($url,$file)
-
-    #(new-object System.Net.WebClient).Downloadfile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B40569D95-7A9C-D7C3-EAF6-1035625105AE%7D%26lang%3Den%26browser%3D2%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/update2/installers/ChromeSetup.exe", "c:\gc_install.exe")
-    #START /WAIT "C:\Windows\Temp\gc_install.exe" -ms
-    msiexec /i "c:\gc_install.exe" /qn /quiet /norestart
+    (new-object System.Net.WebClient).Downloadfile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B40569D95-7A9C-D7C3-EAF6-1035625105AE%7D%26lang%3Den%26browser%3D2%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/update2/installers/ChromeSetup.exe", "gc_install.exe")
+    #START /WAIT "#{Chef::Config[:file_cache_path]}\gc_install.exe" -ms
+    msiexec /i "#{Chef::Config[:file_cache_path]}\gc_install.exe" /qn /quiet /norestart
     #START /WAIT "Firefox Installer"  "c:\Windows\Temp\ff_install.exe" -ms
     #c:\ff_install.exe -ms
   EOH
--- chef-repo/cookbooks/automate_firefox/recipes/default.rb
+++ chef-repo/cookbooks/automate_firefox/recipes/default.rb
@@ -5,11 +5,11 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 powershell_script "Install firefox" do
-  guard_interpreter :powershell_script
-  cwd "c:\Windows\Temp\"
+  cwd Chef::Config[:file_cache_path]
+  environment({'chefc' => Chef::Config[:file_cache_path]})
   code <<-EOH
-    (new-object System.Net.WebClient).Downloadfile("https://download-installer.cdn.mozilla.net/pub/firefox/releases/35.0/win32/en-US/Firefox%20Setup%20Stub%2035.0.exe", "c:\Windows\Temp\ff_install.exe")
-    START /WAIT "C:\Windows\Temp\ff_install.exe" -ms
+    (new-object System.Net.WebClient).Downloadfile("https://download-installer.cdn.mozilla.net/pub/firefox/releases/35.0/win32/en-US/Firefox%20Setup%20Stub%2035.0.exe", "ff_install.exe")
+    START /WAIT "#{Chef::Config[:file_cache_path]}\ff_install.exe" -ms
     #START /WAIT "Firefox Installer"  "c:\Windows\Temp\ff_install.exe" -ms
     #c:\ff_install.exe -ms
   EOH
