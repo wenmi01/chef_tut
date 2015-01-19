aae6c6e6f46dd7561072729c02f96b036348a752
Rowen Remis R. Iral <rowen_remis_iral@yahoo.com>
2015-01-18 22:00:47 -0800

    Succes download and install of firefox msi installer

--- chef-repo/cookbooks/automate_firefox/recipes/default.rb
+++ chef-repo/cookbooks/automate_firefox/recipes/default.rb
@@ -4,15 +4,16 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-powershell_script "Install firefox" do
+powershell_script "Download firefox" do
   cwd Chef::Config[:file_cache_path]
   environment({'chefc' => Chef::Config[:file_cache_path]})
   code <<-EOH
-    (new-object System.Net.WebClient).Downloadfile("http://hicap.frontmotion.com.s3.amazonaws.com/Firefox/Firefox-33.0.2/Firefox-33.0.2-en-US.msi", "ff_install.exe")
-    #START /WAIT "#{Chef::Config[:file_cache_path]}\ff_install.exe" -ms
-    msiexec /i "#{Chef::Config[:file_cache_path]}\ff_install.exe" /qn /quiet /norestart
-    #START /WAIT "Firefox Installer"  "c:\Windows\Temp\ff_install.exe" -ms
-    #c:\ff_install.exe -ms
+    (new-object System.Net.WebClient).Downloadfile("http://hicap.frontmotion.com.s3.amazonaws.com/Firefox/Firefox-33.0.2/Firefox-33.0.2-en-US.msi", "ff_install.msi")
   EOH
   action :run
+end
+
+windows_package "Install Firefox" do
+  source "#{Chef::Config[:file_cache_path]}\\ff_install.msi"
+  action :install
 end
